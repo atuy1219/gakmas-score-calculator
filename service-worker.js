fcd21d6176f3b5aa393f2925eb6a42af88a386c4
Atuy <130917473+atuy1219@users.noreply.github.com>
2026-02-04 14:44:55 +0000

    PWA対応のためのマニフェストとサービスワーカーを追加し、アイコンを追加

---
+++ service-worker.js
@@ -0,0 +1,33 @@
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open('gakmas-cache-v1').then(cache => {
+      return cache.addAll([
+        './',
+        './index.html',
+        './manifest.json',
+        './icon-192.png',
+        './icon-512.png'
+      ]);
+    })
+  );
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', event => {
+  event.waitUntil(
+    caches.keys().then(keys => {
+      return Promise.all(
+        keys.filter(key => key !== 'gakmas-cache-v1').map(key => caches.delete(key))
+      );
+    })
+  );
+  self.clients.claim();
+});
+
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request).then(response => {
+      return response || fetch(event.request);
+    })
+  );
+});
