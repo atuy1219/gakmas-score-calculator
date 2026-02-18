ec873f9d3dcc556265f848af9f93e115bdc1186f
Atuy <130917473+atuy1219@users.noreply.github.com>
2026-02-19 01:14:37 +0900

    Refactor service worker to use constants for cache

--- service-worker.js
+++ service-worker.js
@@ -1,14 +1,16 @@
+const CACHE_NAME = 'gakmas-cache-v1';
+const urlsToCache = [
+  '/gakmas-score-calculator/',
+  '/gakmas-score-calculator/index.html',
+  '/gakmas-score-calculator/manifest.json',
+  '/gakmas-score-calculator/icon-192.png',
+  '/gakmas-score-calculator/icon-512.png'
+];
+
 self.addEventListener('install', event => {
   event.waitUntil(
-    caches.open('gakmas-cache-v1').then(cache => {
-      return cache.addAll([
-        './',
-        './index.html',
-        './manifest.json',
-        './icon-192.png',
-        './icon-512.png'
-      ]);
-    })
+    caches.open(CACHE_NAME)
+      .then(cache => cache.addAll(urlsToCache))
   );
   self.skipWaiting();
 });
@@ -17,7 +19,8 @@ self.addEventListener('activate', event => {
   event.waitUntil(
     caches.keys().then(keys => {
       return Promise.all(
-        keys.filter(key => key !== 'gakmas-cache-v1').map(key => caches.delete(key))
+        keys.filter(key => key !== CACHE_NAME)
+            .map(key => caches.delete(key))
       );
     })
   );
@@ -26,8 +29,7 @@ self.addEventListener('activate', event => {
 
 self.addEventListener('fetch', event => {
   event.respondWith(
-    caches.match(event.request).then(response => {
-      return response || fetch(event.request);
-    })
+    caches.match(event.request)
+      .then(response => response || fetch(event.request))
   );
 });
